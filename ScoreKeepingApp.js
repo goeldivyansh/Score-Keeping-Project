2372264758095f6536e4f5e17f23fb36ceb74134
Divyansh goel <divyanshgoel.19@gmail.com>
2020-06-05 17:27:24 +0530

    update

--- ScoreKeepingApp.js
+++ ScoreKeepingApp.js
@@ -7,6 +7,7 @@ var span2 = document.querySelector("#span2");
 var input = document.querySelector("input");
 var winningScoreDisplay = document.querySelector("#times");
 var resultMsgDisplay = document.querySelector("#resultMsg");
+var resetMsgDisplay = document.querySelector("#resetMsg");
 
 var p1Score = 0;
 var p2Score = 0;
@@ -33,6 +34,7 @@ function p1()
 			span1.classList.add("winner");
 			span1.textContent = p1Score;
 			resultMsgDisplay.textContent = "Player 1 wins!!";
+			resetMsgDisplay.textContent = "Tap Reset to play again!!";
 			gameOver = true;	
 		}	
 	}
@@ -57,6 +59,7 @@ function p2()
 			span2.classList.add("winner");
 			span2.textContent = p2Score;
 			resultMsgDisplay.textContent = "Player 2 wins!!";
+			resetMsgDisplay.textContent = "Tap Reset to play again!";
 			gameOver = true;	
 		}		
 	}
@@ -76,6 +79,7 @@ function reset()
 	span1.classList.remove("winner");
 	span2.classList.remove("winner");
 	resultMsgDisplay.textContent = "";
+	resetMsgDisplay.textContent = "";
 	gameOver=false;
 }
 
